9a29369ed1db4b62b516c9c0ed251cd031868811
qqxpp7 <a903769034@gmail.com>
2024-08-20 12:15:09 +0800

    Update print_change_tag.py

--- code/print_change_tag.py
+++ code/print_change_tag.py
@@ -15,18 +15,38 @@ def main(before_file, after_file, tag_file):
     after_tree = ET.parse(after_file)
     after_root = after_tree.getroot()
     
+    
     with open(tag_file, 'r', encoding='utf-8') as f:
         for line in f:
             tag_path1, tag_path2 = line.strip("<>\n").replace(">/<", "/").split('/')
             
-            b_return = find_a_tag_method(before_root, tag_path1, tag_path2, '')
-            a_return = find_a_tag_method(after_root, tag_path1, tag_path2, '')
-            compare(b_return, a_return, tag_path2)
             
+            b_count = count_a_tag_method(before_root, tag_path1, tag_path2)
+            a_count = count_a_tag_method(after_root, tag_path1, tag_path2)
+            
+            max_count = max(b_count, a_count)
+            
+            if max_count == 1:
+                b_return = find_a_tag_method(before_root, tag_path1, tag_path2, '')
+                a_return = find_a_tag_method(after_root, tag_path1, tag_path2, '')
+                compare(b_return, a_return, tag_path2)
+            else:
+                find_same_tag_method(before_root, tag_path1, tag_path2)
+                
+                
+def count_a_tag_method(node, find_parent_tag, find_child_tag, parent_tag=None):
+    count = 0
+    if node.tag == find_child_tag and parent_tag == find_parent_tag:
+        count += 1
+         
+    for i, child in enumerate(node):        
+        count += count_a_tag_method(child, find_parent_tag, find_child_tag, parent_tag = node.tag)
+        
+    return count            
                                        
 def find_a_tag_method(node, find_parent_tag, find_child_tag, current_path, index=0, parent_tag=None):
     """
-    only can find a tag in 1 times
+    only can find a tag in 1 time
     """
     current_path += f'/{node.tag}[{index}]'
 
@@ -39,6 +59,22 @@ def find_a_tag_method(node, find_parent_tag, find_child_tag, current_path, index
             return result
         
     return None
+
+def find_same_tag_method(node, find_parent_tag, find_child_tag):
+    """
+    find a tag at least 2 times
+    """
+    if node.tag == find_parent_tag:
+        for child_tag in node.findall(find_child_tag):
+            values = [child.text for child in list(child_tag)[:3]]
+            print(f'Book ID {child_tag.get("id")}:', values)   
+    else:        
+        for parent_tag in node.findall('.//find_parent_tag'):
+            for child_tag in parent_tag.findall(find_child_tag):
+                values = [child.text for child in list(child_tag)[:3]]
+                print(f'Book ID {child_tag.get("id")}:', values)
+
+
 
 def compare(before_tag, after_tag, tag_name):
     '''
